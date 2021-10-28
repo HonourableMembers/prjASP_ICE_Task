a6e2875b3bc6fc4031364e353ca9306d6123ad4e
olive <olive@LAPTOP-48LJIM25>
2021-10-28 16:58:09 +0200

    Fixed

--- prjASP_ICE_Task/Default.aspx
+++ prjASP_ICE_Task/Default.aspx
@@ -1,10 +1,12 @@
 ﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="prjASP_ICE_Task._Default" %>
 
+<%@ Register assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" namespace="System.Web.UI.DataVisualization.Charting" tagprefix="asp" %>
+
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
 
     <center>
     <div class="jumbotron">
-        <h1>Welcome, </h1>
+        <h1>Welcome to further depression</h1>
     </div>
 
     <div class="login">
--- prjASP_ICE_Task/Login.aspx.cs
+++ prjASP_ICE_Task/Login.aspx.cs
@@ -6,14 +6,15 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data;
 using System.Data.SqlClient;
-
+using System.Security.Cryptography;
+using System.Text;
 
 namespace prjASP_ICE_Task
 {
     public partial class Login : System.Web.UI.Page
     {
         String connectString = @"Server = LAPTOP-48LJIM25\SQLEXPRESS;" +
-                                "Database = tblUserDetails;" +
+                                "Database = Login;" +
                                 "Integrated Security = True;";
 
         protected void Page_Load(object sender, EventArgs e)
@@ -21,43 +22,25 @@ namespace prjASP_ICE_Task
             lblError.Visible = false;
         }
 
-        protected void btnLogin_Click(object sender, EventArgs e)
+        public string encryptPassword(String password)
         {
-           // try
-            //{
-                using (SqlConnection con = new SqlConnection(connectString))
-                {
-                    con.Open();
-                    String sQuery = "SELECT *" +
-                                    "FROM tblUserDetails" +
-                                    "WHERE UserName = '" + txtUsername.Text + "' " +
-                                    "AND UserPassword = '" + txtPassword.Text + "' ;";
-
-                    SqlCommand command = new SqlCommand(sQuery, con);
-                    SqlDataReader reader = command.ExecuteReader();
-
-                    if (reader.HasRows)
-                    {
-                        Response.Redirect("Default.aspx");                      
-                    }
-                    else
-                    {
-                        lblError.Visible = true;
-                        lblError.Text = "Incorrect user details!" + 
-                                        "\nPlease try again.";
-                    }
-                    reader.Close();
-                    command.Dispose();
-                }
-            //}
-           // catch (Exception)
-           // {
-                
-           // }
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] encrypt;
+            UTF8Encoding encode = new UTF8Encoding();
+            encrypt = md5.ComputeHash(encode.GetBytes(password));
+            StringBuilder encryptdata = new StringBuilder();
+            for (int i = 0; i < encrypt.Length; i++)
+            {
+                encryptdata.Append(encrypt[i].ToString());
+            }
+            return encryptdata.ToString();
         }
 
-        protected void btnRegister_Click(object sender, EventArgs e)
+        protected void btnLogin_Click(object sender, EventArgs e)
         {
+            String sUsername = txtUsername.Text.ToString();
+            String sPassword = txtPassword.Text.ToString();
+
             if (txtUsername.Text.Length <= 0 || txtPassword.Text.Length <= 0)
             {
                 lblError.Visible = true;
@@ -66,14 +49,64 @@ namespace prjASP_ICE_Task
             }
             else
             {
-               // try
-                //{
+                try
+                {
+                    using (SqlConnection con = new SqlConnection(connectString))
+                    {
+                        con.Open();
+                        string encPass = encryptPassword(sPassword);
+                        String sQuery = "SELECT UserName, UserPassword FROM tblUserDetails WHERE (UserName =  '" + sUsername + "') AND (UserPassword = '" + encPass + "');";
+                        SqlCommand command = new SqlCommand(sQuery, con);
+
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        if (reader.HasRows)
+                        {
+                            sUsername = "";
+                            sPassword = "";
+                            Response.Redirect("Default.aspx");
+                        }
+                        else
+                        {
+                            lblError.Visible = true;
+                            lblError.Text = "Incorrect user details!" +
+                                            "\nPlease try again.";
+                        }
+                        reader.Close();
+                        command.Dispose();
+                    }
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            String sUsername = txtUsername.Text.ToString();
+            String sPassword = txtPassword.Text.ToString();
+            String encPass = encryptPassword(sPassword);
+
+            if (txtUsername.Text.Length <= 0 || txtPassword.Text.Length <= 0)
+            {
+                lblError.Visible = true;
+                lblError.Text = "There can't be empty fields!" +
+                                "\nPlease fill in the field with your details.";
+            }
+            else
+            {
+                try
+                {
                     using (SqlConnection connection = new SqlConnection(connectString))
                     {
                         SqlCommand command = new SqlCommand("INSERT INTO tblUserDetails " +
                                                             "VALUES(@UserName, @UserPassword) ;", connection);
-                        command.Parameters.AddWithValue("@UserName", txtUsername.Text);
-                        command.Parameters.AddWithValue("@UserPassword", txtPassword.Text);
+                        command.Parameters.AddWithValue("@UserName", sUsername);
+                        command.Parameters.AddWithValue("@UserPassword", encPass);
+                        String passwords = encryptPassword(sPassword);
+
                         connection.Open();
 
                         SqlDataAdapter adapter = new SqlDataAdapter();
@@ -81,16 +114,17 @@ namespace prjASP_ICE_Task
 
                         int id = Convert.ToInt32(adapter.InsertCommand.ExecuteScalar());
                         lblError.Visible = true;
+                        lblError.ForeColor = System.Drawing.Color.Black;
                         lblError.Text = "New user successfully added";
                         adapter.Dispose();
                     }
-                //}
-               //catch (Exception)
-                //{
+                }
+               catch (Exception)
+                {
 
-               // }
-                txtPassword.Text = "";
+                }
                 txtUsername.Text = "";
+                txtPassword.Text = "";
             }
         }
     }
